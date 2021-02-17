2dc4b0cf49569eb3adad21262c2ee6ab6dc42238
tomoki <tomoki171923@gmail.com>
2021-02-17 19:21:53 +0900

    add encode/decode functions in convert.py

--- convert.py
+++ convert.py
@@ -1,6 +1,9 @@
 # -*- coding: utf-8 -*-
 
 import ast
+import pickle
+import base64
+import json
 
 ''' convert String to List.
 Args:
@@ -37,3 +40,33 @@ e.g.2
 
 def strToDict(obj: str):
     return ast.literal_eval(obj)
+
+
+''' convert Dict to Bytes. (Endode)
+Args:
+    obj (dict): the target obj.
+Returns:
+    bytes: the target obj.
+e.g.
+    obj = {"key1": "value1", "key2": 123, "key3": "https://www.google.com/"}
+        # => b'eyJrZXkxIjogInZhbHVlMSIsICJrZXkyIjogMTIzLCAia2V5MyI6ICJodHRwczovL3d3dy5nb29nbGUuY29tLyJ9'
+'''
+
+
+def dictToBytes(obj: dict):
+    return base64.urlsafe_b64encode(json.dumps(obj).encode())
+
+
+''' convert Bytes to Dict. (Dedode)
+Args:
+    obj (bytes): the target obj.
+Returns:
+    dict: the target obj.
+e.g.
+    obj = b'eyJrZXkxIjogInZhbHVlMSIsICJrZXkyIjogMTIzLCAia2V5MyI6ICJodHRwczovL3d3dy5nb29nbGUuY29tLyJ9'
+        # => {"key1": "value1", "key2": 123, "key3": "https://www.google.com/"}
+'''
+
+
+def bytesToDict(obj: bytes):
+    return base64.urlsafe_b64decode(obj).decode()
