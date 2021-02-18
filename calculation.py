07bd9d1b58cc17fe6aeaccb067ef108c71279cee
tomoki <tomoki171923@gmail.com>
2021-02-18 14:01:32 +0900

    refacter & add ut code for calculation.py

--- calculation.py
+++ calculation.py
@@ -3,68 +3,60 @@
 from decimal import Decimal, ROUND_HALF_UP, ROUND_DOWN
 
 ''' round up the number to the nearest integer.
-e.g. from 123.456 to 123
-e.g. from 123.567 to 124
+e.g. num = 123.456 # => 123
+e.g. num = 123.567 # => 124
 
 Args:
-    num (Decimal): the target number.
+    num (float): the target number.
 Returns:
     int: the number rounded up to the nearest integer
 '''
 
 
-def roundUp1(num: Decimal):
-    if type(num) is not Decimal:
-        num = Decimal(num)
-    return int(num.quantize(Decimal('1'), rounding=ROUND_HALF_UP))
+def roundUp1(num: float) -> int:
+    return int(Decimal(num).quantize(Decimal('1'), rounding=ROUND_HALF_UP))
 
 
 ''' round up the number to the nearest ten.
-e.g. from 1234.56 to 1230
-e.g. from 1235.67 to 1240
+e.g. num = 1234.56 # => 1230
+e.g. num = 1235.67 # => 1240
 
 Args:
-    num (Decimal): the target number.
+    num (float): the target number.
 Returns:
     int: the number rounded up to the nearest ten
 '''
 
 
-def roundUp10(num: Decimal):
-    if type(num) is not Decimal:
-        num = Decimal(num)
-    return int(num.quantize(Decimal('1E1'), rounding=ROUND_HALF_UP))
+def roundUp10(num: float) -> int:
+    return int(Decimal(num).quantize(Decimal('1E1'), rounding=ROUND_HALF_UP))
 
 
 ''' round down the number to the nearest integer.
-e.g. from 123.456 to 123
-e.g. from 123.567 to 123
+e.g. num = 123.456 # => 123
+e.g. num = 123.567 # => 123
 
 Args:
-    num (Decimal): the target number.
+    num (float): the target number.
 Returns:
     int: the number rounded down to the nearest integer
 '''
 
 
-def roundDown1(num: Decimal):
-    if type(num) is not Decimal:
-        num = Decimal(num)
-    return int(num.quantize(Decimal('1'), rounding=ROUND_DOWN))
+def roundDown1(num: float) -> int:
+    return int(Decimal(num).quantize(Decimal('1'), rounding=ROUND_DOWN))
 
 
 ''' round down the number to the nearest ten.
-e.g. from 1234.56 to 1230
-e.g. from 1235.67 to 1230
+e.g. num = 1234.56 # => 1230
+e.g. num = 1235.67 # => 1230
 
 Args:
-    num (Decimal): the target number.
+    num (float): the target number.
 Returns:
     int: the number rounded down to the nearest ten
 '''
 
 
-def roundDown10(num: Decimal):
-    if type(num) is not Decimal:
-        num = Decimal(num)
-    return int(num.quantize(Decimal('1E1'), rounding=ROUND_DOWN))
+def roundDown10(num: float) -> int:
+    return int(Decimal(num).quantize(Decimal('1E1'), rounding=ROUND_DOWN))
---
+++ ut_calculation.py
@@ -0,0 +1,110 @@
+import unittest
+
+from calculation import roundUp1, roundUp10, roundDown1, roundDown10
+import decimal
+
+
+class UtCalculation(unittest.TestCase):
+    # constructor of unittest class
+    @classmethod
+    def setUpClass(self):
+        pass
+
+    # destructor of unittest class
+    @classmethod
+    def tearDownClass(self):
+        pass
+
+    # the action before each of tests is executed in unittest
+    def setUp(self):
+        pass
+
+    # the action after each of tests is executed in unittest
+    def tearDown(self):
+        pass
+
+    def test_roundUp1_case1(self):
+        ut_data: float = 123.456
+        expected_result = 123
+        result = roundUp1(ut_data)
+        # type test
+        self.assertIs(type(result), int)
+        # value test
+        self.assertEqual(expected_result, result)
+
+    def test_roundUp1_case2(self):
+        ut_data: float = 123.567
+        expected_result: int = 124
+        result = roundUp1(ut_data)
+        # type test
+        self.assertIs(type(result), int)
+        # value test
+        self.assertEqual(expected_result, result)
+
+    def test_roundUp10_case1(self):
+        ut_data: float = 1234.56
+        expected_result: int = 1230
+        result = roundUp10(ut_data)
+        # type test
+        self.assertIs(type(result), int)
+        # value test
+        self.assertEqual(expected_result, result)
+
+    def test_roundUp10_case2(self):
+        ut_data: float = 1235.67
+        expected_result: int = 1240
+        result = roundUp10(ut_data)
+        # type test
+        self.assertIs(type(result), int)
+        # value test
+        self.assertEqual(expected_result, result)
+
+    def test_roundDown1_case1(self):
+        ut_data: float = 123.456
+        expected_result = 123
+        result = roundDown1(ut_data)
+        # type test
+        self.assertIs(type(result), int)
+        # value test
+        self.assertEqual(expected_result, result)
+
+    def test_roundDown1_case2(self):
+        ut_data: float = 123.567
+        expected_result: int = 123
+        result = roundDown1(ut_data)
+        # type test
+        self.assertIs(type(result), int)
+        # value test
+        self.assertEqual(expected_result, result)
+
+    def test_roundDown10_case1(self):
+        ut_data: float = 1234.56
+        expected_result = 1230
+        result = roundDown10(ut_data)
+        # type test
+        self.assertIs(type(result), int)
+        # value test
+        self.assertEqual(expected_result, result)
+
+    def test_roundDown10_case2(self):
+        ut_data: float = 1235.67
+        expected_result: int = 1230
+        result = roundDown10(ut_data)
+        # type test
+        self.assertIs(type(result), int)
+        # value test
+        self.assertEqual(expected_result, result)
+
+    def test_args(self):
+        with self.assertRaises(decimal.InvalidOperation):
+            roundUp1('string')
+        with self.assertRaises(decimal.InvalidOperation):
+            roundUp10('string')
+        with self.assertRaises(decimal.InvalidOperation):
+            roundDown1('string')
+        with self.assertRaises(decimal.InvalidOperation):
+            roundDown10('string')
+
+
+if __name__ == "__main__":
+    unittest.main()
